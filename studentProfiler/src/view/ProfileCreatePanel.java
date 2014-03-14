16892d39968aa590b5bf0900744c74a7fb55328d
Scrimley <bajgerrathter@googlemail.com>
2014-03-14 15:34:15 +0000

    updated ProfileCreatePanel

--- studentProfiler/src/view/ProfileCreatePanel.java
+++ studentProfiler/src/view/ProfileCreatePanel.java
@@ -1,11 +1,17 @@
 package view;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 import model.Course;
 import model.Module;
@@ -16,6 +22,7 @@ public class ProfileCreatePanel extends JPanel {
 	private JTextField name;
 	private JTextField pnumber;
 	private JButton createButton;
+	private GridLayout experimentLayout = new GridLayout(0,2,10,5);
 
 	public ProfileCreatePanel() {
 		
@@ -23,44 +30,36 @@ public class ProfileCreatePanel extends JPanel {
 		Course[] courses = setupAndGetCourses();
 		populateComboBox(courses);
 		
-		Box courseBox = Box.createHorizontalBox();
-		courseBox.add(new JLabel("Select course: "));
-		courseBox.add(Box.createHorizontalStrut(10));
-		courseBox.add(cboCourses);
-		courseBox.add(Box.createHorizontalStrut(10));
-		
 		name = new JTextField();
-		
-		Box nameBox = Box.createHorizontalBox();
-		nameBox.add(new JLabel("Input full name:"));
-		nameBox.add(Box.createHorizontalStrut(10));
-        nameBox.add(name);
-        nameBox.add(Box.createHorizontalStrut(10));
         
         pnumber = new JTextField();
-		
-		Box pnumberBox = Box.createHorizontalBox();
-		pnumberBox.add(new JLabel("Input P number:"));
-		pnumberBox.add(Box.createHorizontalStrut(10));
-		pnumberBox.add(pnumber);
-		pnumberBox.add(Box.createHorizontalStrut(10));
-		
 		
 		createButton = new JButton("Create Profile");
 		Box buttonBox = Box.createHorizontalBox();
 		buttonBox.add(createButton);
 		
-		Box profileBox = Box.createVerticalBox();
-		profileBox.add(Box.createVerticalStrut(10));
-	    profileBox.add(courseBox);
-		profileBox.add(Box.createVerticalStrut(10));
-        profileBox.add(nameBox);
-        profileBox.add(Box.createVerticalStrut(10));
-        profileBox.add(pnumberBox);
-        profileBox.add(Box.createVerticalStrut(10));
-        profileBox.add(buttonBox);
+		final JPanel profileGrid = new JPanel();
 		
-        this.add(profileBox);
+		//make labels
+		JLabel courselbl = new JLabel("Select course:", SwingConstants.RIGHT);
+		JLabel namelbl = new JLabel("Input full name:", SwingConstants.RIGHT);
+		JLabel pnumberlbl = new JLabel("Input P number:", SwingConstants.RIGHT);
+
+		profileGrid.setLayout(experimentLayout);
+	    profileGrid.add(courselbl);
+        profileGrid.add(cboCourses);
+        profileGrid.add(namelbl);
+        profileGrid.add(name);
+        profileGrid.add(pnumberlbl);
+        profileGrid.add(pnumber);
+        
+        Box outerBox = Box.createVerticalBox();
+        outerBox.add(Box.createVerticalStrut(10), BorderLayout.NORTH);
+        outerBox.add(profileGrid, BorderLayout.CENTER);
+        outerBox.add(Box.createVerticalStrut(10), BorderLayout.SOUTH);
+        outerBox.add(buttonBox, BorderLayout.SOUTH);
+        
+        this.add(outerBox);
 	}
 
 	public void populateComboBox(Course[] courses) { 
@@ -69,7 +68,7 @@ public class ProfileCreatePanel extends JPanel {
 		} 
 	} 
 
-	private Course[] setupAndGetCourses() { 
+	private Course[] setupAndGetCourses() { //to be moved into controller
 		Module ctec2121 = new Module("CTEC2121", "Organisations, Project Management and Research"); 
 		Module ctec2122 = new Module("CTEC2122", "Forensics and Security"); 
 		Module ctec2602 = new Module("CTEC2602", "OO Software Design and Development"); 
